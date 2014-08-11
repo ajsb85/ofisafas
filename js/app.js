40f7746e9151a71c0ecec9110fedf605936631d5
Alexander Salas <a.salas@ieee.org>
2014-08-11 18:56:43 -0430

    More test xD

--- js/app.js
+++ js/app.js
@@ -281,3 +281,57 @@ myApp.onPageInit('*', function (page) {
 	Transifex.live.translateNode($('.view-main').get(0));
 	promoBrowserPage.expositionOff();
 });
+
+	// Loading flag
+	var loading = false;
+
+	// Last loaded index
+	
+
+	// Max items to load
+	var maxItems = 60;
+
+	// Append items per load
+	var itemsPerLoad = 20;
+
+//Sometimes we may need the same callback for few pages. We may separate page names with space:
+myApp.onPageInit('news', function (page) {
+	var lastIndex = $$('.ok li').length;
+	console.log(lastIndex);
+	// Attach 'infinite' event handler
+	$$('.infinite-scroll').on('infinite', function () {
+
+		// Exit, if loading in progress
+		if (loading) return;
+
+		// Set loading flag
+		loading = true;
+
+		// Emulate 1s loading
+		setTimeout(function () {
+			// Reset loading flag
+			loading = false;
+
+			if (lastIndex >= maxItems) {
+				// Nothing more to load, detach infinite scroll events to prevent unnecessary loadings
+				myApp.detachInfiniteScroll($$('.infinite-scroll'));
+				// Remove preloader
+				$$('.infinite-scroll-preloader').remove();
+				return;
+			}
+
+			// Generate new items HTML
+			var html = '';
+			for (var i = lastIndex + 1; i <= lastIndex + itemsPerLoad; i++) {
+				html += '<li class="item-content"><div class="item-inner"><div class="item-title">Item ' + i + '</div></div></li>';
+			}
+
+			// Append new items
+			$$('.time-line-content').append(html);
+
+			// Update last loaded index
+			lastIndex = $$('.ok li').length;
+		}, 1000);
+	});   
+});
+ 
